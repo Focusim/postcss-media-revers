6a4d9089df2f4d8f48d262919b2bb50eb8469c20
RomanG <r.garanin@intensa.ru>
2023-06-02 14:50:59 +0300

    refactor

--- index.js
+++ index.js
@@ -1,46 +1,29 @@
 import * as postcss from 'postcss';
 
-/**
- * @type {import('postcss').PluginCreator}
- */
-module.exports = (opts = {}) => {
+export default postcss.plugin('postcss-reverse-media', (options = {}) => {
   // Work with options here
+  return css => {
+    // Transform each rule here
+    /*
+      css.walkDecls(rule => {
 
-  return {
-    postcssPlugin: 'postcss-media-revers',
-
-    Once(css) {
-      css.nodes.forEach(el => {
-        if (el.type === 'rule') {
+        if (rule.type) {
           css.append(postcss.atRule({
-            params: '(min-width: 1281px)',
-            name: 'media',
-            nodes: [el]
+              params: rule.type,
+              name: 'media',
           }));
         }
-      })
-    }
-
-    /*
-    Root (root, postcss) {
-      // Transform CSS AST here
-    }
+      });
     */
 
-    /*
-    Declaration (decl, postcss) {
-      // The faster way to find Declaration node
-    }
-    */
-
-    /*
-    Declaration: {
-      color: (decl, postcss) {
-        // The fastest way find Declaration node if you know property name
+    css.nodes.forEach(el => {
+      if (el.type === 'rule') {
+        css.append(postcss.atRule({
+          params: '(min-width: 1281px)',
+          name: 'media',
+          nodes: [el]
+        }));
       }
-    }
-    */
-  }
-}
-
-module.exports.postcss = true
+    })
+  };
+});
